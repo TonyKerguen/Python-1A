01c80815060959da21620ce34795901e28b8a740
Kerguen Tony <o22205496@info03-31.iut45.univ-orleans.fr>
2023-11-07 16:53:25 +0100

    TP8a

--- TP8a/motdepasse.py
+++ TP8a/motdepasse.py
@@ -1,35 +1,124 @@
 # Codé par Papy Force X, jeune padawan de l'informatique
 
 def dialogue_mot_de_passe():
+    """indique si son mot de passe est correct
+
+    Returns:
+        str: le mot de passe
+    """    
     login = input("Entrez votre nom : ")
     mot_de_passe_correct = False
     while not mot_de_passe_correct :
         mot_de_passe = input("Entrez votre mot de passe : ")
         # je vérifie la longueur
-        if len(mot_de_passe) < 8:
-            longueur_ok = False;
-        else:
-            longueur_ok = True
+        longueur_ok_check = longueur_ok(mot_de_passe)
         # je vérifie s'il y a un chiffre
-        chiffre_ok = False
-        for lettre in mot_de_passe:
-            if lettre.isdigit():
-                chiffre_ok = True
+        chiffre_ok_check = chiffre_ok(mot_de_passe)
         # je vérifie qu'il n'y a pas d'espace
-        sans_espace = True
-        for lettre in mot_de_passe:
-            if lettre == " ":
-                sans_espace = False
+        sans_espace_check = sans_espace(mot_de_passe)
+        # je vérifie qu'il n'y a pas 2 chiffre consecutif
+        sans_deux_chiffre_consecutif_check = sans_deux_chiffre_consecutif(mot_de_passe)
+        # je vérifie que le plus petit chiffre apparait une seul fois
+        plus_petit_chiffre_apparait_une_seul_fois_check = plus_petit_chiffre_apparait_une_seul_fois(mot_de_passe)
         # Je gère l'affichage
-        if not longueur_ok:
+        if not longueur_ok_check:
             print("Votre mot de passe doit comporter au moins 8 caractères")
-        elif not chiffre_ok:
-            print("Votre mot de passe doit comporter au moins un chiffre")
-        elif not sans_espace:
-            print("Votre mot de passe ne doit pas comporter d'espace")	   
+        elif not chiffre_ok_check:
+            print("Votre mot de passe doit comporter au moins 3 chiffres")
+        elif not sans_espace_check:
+            print("Votre mot de passe ne doit pas comporter d'espace")
+        elif not sans_deux_chiffre_consecutif_check:
+            print("Votre mot de passe ne doit pas contenir 2 chiffre consecutif")
+        elif not plus_petit_chiffre_apparait_une_seul_fois_check:
+            print("Votre mot de passe ne doit pas contenir le plus petit chiffre plusieurs fois") 
         else:
             mot_de_passe_correct = True        
     print("Votre mot de passe est correct")
     return mot_de_passe
+
+
+def longueur_ok(chaine):
+    """verifie qu'une chaine fait plus de 8 caracteres
+
+    Args:
+        chaine (str): une chaine de caracteres
+
+    Returns:
+        bool: True si la longueur de la chaine fait plus de 8 caracteres, False sinon
+    """    
+    return len(chaine) >= 8
+
+
+def chiffre_ok(chaine):
+    """verifie qu'une chaine de caracteres contient au moins 3 chiffres
+
+    Args:
+        chaine (str): une chaine de caracteres
+
+    Returns:
+        bool: True si la chaine contient au moins 3 chiffres, False sinon
+    """
+    nb_chiffre = 0
+    for lettre in chaine:
+        if lettre.isdigit():
+            nb_chiffre += 1
+    return nb_chiffre >= 3
+
+def sans_espace(chaine):
+    """verifie qu'une chaine de caracteres ne contient pas d'espace
+
+    Args:
+        chaine (str): une chaine de caracteres
+
+    Returns:
+        bool: True si la chaine ne contient pas d'espace, False sinon
+    """
+    for lettre in chaine:
+        if lettre == " ":
+            return False
+    return True
+
+def sans_deux_chiffre_consecutif(chaine):
+    """verifie qu'une chaine ne contient pas 2 chiffre consecutif
+
+    Args:
+        chaine (str): une chaine de caracteres
+
+    Returns:
+        bool: True si la chaine ne contient pas 2 chiffre consecutif, False sinon
+    """    
+    if len(chaine) > 1:
+        for lettre in range(1, len(chaine)):
+             if chaine[lettre].isdigit() and chaine[lettre-1].isdigit():
+                  return False
+        return True
+    return False
+
+
+def plus_petit_chiffre_apparait_une_seul_fois(chaine):
+    """verifie qu'une chaine ne contient qu'une seul fois le plus petit chiffre
+
+    Args:
+        chaine (str): une chaine de caracteres
+
+    Returns:
+        bool: True si la chaine ne contient qu'une seul fois le plus petit chiffre, False sinon
+    """    
+    nb_apparition_plus_petit_nombre = 0
+    plus_petit_nombre = None
+    for lettre in chaine:
+        if lettre.isdigit():
+            if plus_petit_nombre == None:
+                plus_petit_nombre = lettre
+                nb_apparition_plus_petit_nombre = 1
+            elif lettre < plus_petit_nombre:
+                plus_petit_nombre = lettre
+                nb_apparition_plus_petit_nombre = 1
+            elif lettre == plus_petit_nombre:
+                nb_apparition_plus_petit_nombre += 1
+    return nb_apparition_plus_petit_nombre == 1
+
+
+
 
 dialogue_mot_de_passe()
--- TP8a/test_motdepasse.py
+++ TP8a/test_motdepasse.py
@@ -11,13 +11,13 @@ def test_longueur_ok():
 
 
 def test_chiffre_ok():
-    assert motdepasse.chiffre_ok("chou9bouilli")  # chiffre au milieu
-    assert motdepasse.chiffre_ok("7choubouilli")  # chiffre au début
-    assert motdepasse.chiffre_ok("choubouilli5")  # chiffre à la fin
-    assert motdepasse.chiffre_ok("chou3boui8lli")  # deux chiffres    
+    assert not motdepasse.chiffre_ok("chou9bouilli")  # chiffre au milieu
+    assert not motdepasse.chiffre_ok("7choubouilli")  # chiffre au début
+    assert not motdepasse.chiffre_ok("choubouilli5")  # chiffre à la fin
+    assert not motdepasse.chiffre_ok("chou3boui8lli")  # deux chiffres    
     assert not motdepasse.chiffre_ok("chou")       # pas de chiffres
     assert not motdepasse.chiffre_ok("un deux trois") # pas de chiffres
-
+    assert motdepasse.chiffre_ok("chou3boui8lli4")  # trois chiffres   
 
 
 def test_sans_espace():
@@ -28,3 +28,14 @@ def test_sans_espace():
     assert motdepasse.sans_espace("") # chaine vide
 
 
+def test_sans_deux_chiffre_consecutif():
+    assert not motdepasse.sans_deux_chiffre_consecutif("a")
+    assert motdepasse.sans_deux_chiffre_consecutif("aa")
+    assert not motdepasse.sans_deux_chiffre_consecutif("a11")
+    assert motdepasse.sans_deux_chiffre_consecutif("1a1")
+
+def test_plus_petit_chiffre_apparait_une_seul_fois():
+    assert not motdepasse.plus_petit_chiffre_apparait_une_seul_fois("aaa")
+    assert not motdepasse.plus_petit_chiffre_apparait_une_seul_fois("1a2a3a1")
+    assert motdepasse.plus_petit_chiffre_apparait_une_seul_fois("1a2a3a3")
+    assert motdepasse.plus_petit_chiffre_apparait_une_seul_fois("1a2202a3a3")
