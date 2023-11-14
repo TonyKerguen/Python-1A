0f164ee89ca2badac9aa828a71555cc31620a397
Kerguen Tony <o22205496@info03-31.iut45.univ-orleans.fr>
2023-11-14 16:58:26 +0100

    TP9

--- TP9/1_4_petites_betes/petites_betes.py
+++ TP9/1_4_petites_betes/petites_betes.py
@@ -1,6 +1,5 @@
 """Init Dev : TP9"""
 
-
 # ==========================
 # Petites bêtes
 # ==========================
@@ -15,7 +14,10 @@ def toutes_les_familles(pokedex):
     Returns:
         set: l'ensemble des familles représentées dans le pokedex
     """
-    ...
+    res = set()
+    for (_, famille) in pokedex:
+        res.add(famille)
+    return res
 
 def nombre_pokemons(pokedex, famille):
     """calcule le nombre de pokemons d'une certaine famille dans un pokedex
@@ -28,7 +30,11 @@ def nombre_pokemons(pokedex, famille):
     Returns:
         int: le nombre de pokemons d'une certaine famille dans un pokedex
     """
-    ...
+    res = 0
+    for (_, type) in pokedex:
+        if famille == type:
+            res+=1
+    return res
 
 def frequences_famille(pokedex):
     """Construit le dictionnaire de fréqeunces des familles d'un pokedex
@@ -41,7 +47,10 @@ def frequences_famille(pokedex):
         dict: un dictionnaire dont les clés sont le nom de familles (str)
         et la valeur associée est le nombre de représentants de la famille (int)
     """
-    ...
+    res = dict()
+    for (_, type) in pokedex:
+        res[type] = nombre_pokemons(pokedex, type)
+    return res
 
 def dico_par_famille(pokedex):
     """Construit un dictionnaire dont les les clés sont le nom de familles (str)
@@ -56,7 +65,15 @@ def dico_par_famille(pokedex):
         dict: un dictionnaire dont les clés sont le nom de familles (str) et la valeur associée est
         l'ensemble (set) des noms des pokemons de cette famille dans le pokedex
     """
-    ...
+    res = dict()
+    familles = toutes_les_familles(pokedex)
+    for famille in familles:
+        noms_pokes = set()
+        for (nom, type) in pokedex:
+            if type == famille:
+                noms_pokes.add(nom)
+        res[famille] = noms_pokes
+    return res
 
 def famille_la_plus_representee(pokedex):
     """détermine le nom de la famille la plus représentée dans le pokedex
@@ -68,13 +85,19 @@ def famille_la_plus_representee(pokedex):
     Returns:
         str: le nom de la famille la plus représentée dans le pokedex
     """
-    ...
-
+    res = None
+    nb_max = 0
+    familles = toutes_les_familles(pokedex)
+    for famille in familles:
+        nb_poke_famille = nombre_pokemons(pokedex, famille)
+        if res is None or nb_poke_famille > nb_max:
+            res = famille
+            nb_max = nb_poke_famille
+    return res
 
 # ==========================
 # Petites bêtes (la suite)
 # ==========================
-
 
 def toutes_les_familles_v2(pokedex):
     """détermine l'ensemble des familles représentées dans le pokedex
@@ -86,7 +109,11 @@ def toutes_les_familles_v2(pokedex):
     Returns:
         set: l'ensemble des familles représentées dans le pokedex
     """
-    ...
+    res = set()
+    for type in pokedex.values():
+        for type2 in type:
+            res.add(type2)
+    return res
 
 def nombre_pokemons_v2(pokedex, famille):
     """calcule le nombre de pokemons d'une certaine famille dans un pokedex
@@ -99,7 +126,11 @@ def nombre_pokemons_v2(pokedex, famille):
     Returns:
         int: le nombre de pokemons d'une certaine famille dans un pokedex
     """
-    ...
+    res = 0
+    for type in pokedex.values():
+        if famille in type:
+            res += 1
+    return res
 
 def frequences_famille_v2(pokedex):
     """Construit le dictionnaire de fréqeunces des familles d'un pokedex
@@ -112,7 +143,11 @@ def frequences_famille_v2(pokedex):
         dict: un dictionnaire dont les clés sont le nom de familles (str) et la valeur
         associée est le nombre de représentants de la famille (int)
     """
-    ...
+    res = dict()
+    familles = toutes_les_familles_v2(pokedex)
+    for famille in familles:
+        res[famille] = nombre_pokemons_v2(pokedex, famille)
+    return res
 
 def dico_par_famille_v2(pokedex):
     """Construit un dictionnaire dont les les clés sont le nom de familles (str)
@@ -127,7 +162,15 @@ def dico_par_famille_v2(pokedex):
         dict: un dictionnaire dont les clés sont le nom de familles (str) et la valeur associée est
         l'ensemble (set) des noms des pokemons de cette famille dans le pokedex
     """
-    ...
+    res = dict()
+    familles = toutes_les_familles_v2(pokedex)
+    for famille in familles:
+        noms_pokes = set()
+        for (nom, type) in pokedex.items():
+            if famille in type:
+                noms_pokes.add(nom)
+        res[famille] = noms_pokes
+    return res
 
 def famille_la_plus_representee_v2(pokedex):
     """détermine le nom de la famille la plus représentée dans le pokedex
